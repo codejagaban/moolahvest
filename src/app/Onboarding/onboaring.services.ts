9208dee6f86689cfbc912844c001cd3d8d173437
UOghenejakpor@polarisbanklimited.com <UOghenejakpor@polarisbanklimited.com>
2020-10-12 10:08:22 +0100

    updates on onboarding services

--- src/app/Onboarding/onboaring.services.ts
+++ src/app/Onboarding/onboaring.services.ts
@@ -3,31 +3,32 @@ import { ApiService } from 'src/app/shared/services/Api.services';
 import { Injectable } from '@angular/core';
 import { Config } from 'src/assets/config/config';
 import { map } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn :'root'
+  providedIn : 'root'
 })
 export class OnboardingService extends ApiService<any> {
-baseUrl: string = `${Config.settings.apiServer.SavingsUrl}/onboarding`;
+baseUrl = `${Config.settings.apiServer.OnboardingUrl}`;
 
-public user:any;
+public user: any= new BehaviorSubject(null);
   constructor(httpClient: HttpClient) {
     super(httpClient);
   }
   withoutBvn(user: any ) {
     return this.create(user, `${this.baseUrl}/withoutBVN`).pipe(
-      map(user=> {
-       this.user = user;
+      map( response => {
+       this.user = response.payload;
       })
     );
   }
 validateOTP(model: any ){
-    return this.create(model,`${Config.settings.apiServer.SavingsUrl}/validateOTP`);
+    return this.create(model, `${Config.settings.apiServer.SavingsUrl}/validateOTP`);
 }
 resentOTP(model: any){
-    return this.create(model,`${Config.settings.apiServer.SavingsUrl}/ResendOTP`);
+    return this.create(model, `${Config.settings.apiServer.SavingsUrl}/ResendOTP`);
 }
-setPassword(model:any){
-    return this.create(model,`${Config.settings.apiServer.SavingsUrl}/SetPassword`);
+setPassword(model: any){
+    return this.create(model, `${Config.settings.apiServer.SavingsUrl}/SetPassword`);
 }
 }
