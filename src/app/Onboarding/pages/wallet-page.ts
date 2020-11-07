557e5ccf8de766f212fabf0c0196f88f3b1b8b56
codekyd <okpusjamin@gmail.com>
2020-11-07 07:19:55 +0100

    styles: refactor styles and global style for reuseablility

--- src/app/Onboarding/pages/wallet-page.ts
+++ src/app/Onboarding/pages/wallet-page.ts
@@ -2,16 +2,16 @@ import { FormGroup } from '@angular/forms';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-	selector: "app-onboard-wallet",
+	selector: 'app-onboard-wallet',
 	styleUrls: ['../styles/wallet.scss'],
 	templateUrl: '../views/wallet.html'
 })
 export class WalletPage implements OnInit{
-	@Input() onboardForm: FormGroup;
+	@Input() boardForm: FormGroup;
 	constructor() {  }
 	ngOnInit() { }
 	onClick(){
-		console.log(this.onboardForm);
+		console.log(this.boardForm);
 	}
 
 }
