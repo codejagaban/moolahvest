2fbc27d6b88678b651d4d9b2e1a8c6147d6c51b0
codekyd <okpusjamin@gmail.com>
2020-11-12 08:18:38 +0100

    chore(stepper): hide header on mobile view based on the current step

--- src/app/Shared/components/stepper.page.ts
+++ src/app/Shared/components/stepper.page.ts
@@ -3,19 +3,19 @@ import { Component, Input, OnInit } from '@angular/core';
 
 
 @Component({
-    selector: 'app-stepper',
-	styleUrls: ['../styles/stepper.scss'],
-    templateUrl: '../views/stepper.html',
-    providers: [{provide: CdkStepper, useExisting: StepperPage}]
+  selector: 'app-stepper',
+  styleUrls: ['../styles/stepper.scss'],
+  templateUrl: '../views/stepper.html',
+  providers: [{provide: CdkStepper, useExisting: StepperPage}]
 })
 export class StepperPage extends CdkStepper implements OnInit {
-    @Input() linerModeSelected: boolean;
-    ngOnInit(): void {
-        this.linear = this.linerModeSelected;
-    }
+  @Input() linerModeSelected: boolean;
+  ngOnInit(): void {
+      this.linear = this.linerModeSelected;
+  }
 
-    onClick(index: number){
-        this.selectedIndex = index;
-        console.log(this.selectedIndex);
-    }
+  onClick(index: number){
+      this.selectedIndex = index;
+      console.log(this.selectedIndex);
+  }
 }
